4881735f775bb5bf94ea72cf14eccde493de093c
Jaya Balan A <jb@localhost.localdomain>
2016-12-17 19:44:53 +0530

    usb bootable

---
+++ bootable.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+LNX_ver=3.10.0-327.el7.x86_64
+LNX_dev=/dev/sdb
+LNX_rootdev=/dev/sdb5
+LNX_bootdev=/dev/sdb1 #TODO include in fstab
+
+LNX_boot_hdd=/dev/sda #asuming this is the first disk
+
+LTMPDIR=/mnt/ltmpdir
+
+dpart(){
+  pdev=$1
+  echo "[I] creating partitions on [$pdev] for fresh isntall" 
+  parted -s ${pdev} mklabel msdos
+  parted -a optimal --script ${pdev} \
+    unit MiB \
+    mkpart primary ext2 0% 2% \
+    mkpart extended 2% 100% \
+    mkpart logical ext4 2% 100% \
+    set 1 boot on \
+    print \
+    quit
+}
+dformat(){
+  echo "[I] formatting partitions on [$pdev] for fresh isntall" 
+  pdev=$1
+
+  ls -l ${pdev}1
+  ls -l ${pdev}5
+
+  mkfs -t ext2 -F ${pdev}1
+  mkfs -t ext4 -F ${pdev}5
+}
+install_extlinux(){
+  LNX_image=vmlinuz-$LNX_ver
+  LNX_initrd=initramfs-${LNX_ver}.img
+  EXTDIR=$LTMPDIR/boot/extlinux
+
+  #- install bootloader
+  mkdir -p $EXTDIR
+  echo "[I] extlinux =>  $LNX_dev -> $EXTDIR"
+  
+  #- update bootloader config
+  #-- qemu default: root=/dev/sda
+  cat > $EXTDIR/extlinux.conf <<- EOM 
+  DEFAULT menu.c32
+  PROMPT 1
+  LABEL   $LNX_image
+  SAY     Booting - $LNX_image 123
+  LINUX   /$LNX_image
+  INITRD  /$LNX_initrd
+  APPEND  root=${LNX_boot_hdd}5 ro
+EOM
+  cat $EXTDIR/extlinux.conf
+
+  extlinux --install $EXTDIR 
+
+  MBR_bin=/usr/share/syslinux/mbr.bin 
+  echo "[I] extlinux = $MBR_bin"
+  dd if=${MBR_bin} conv=notrunc bs=440 count=1 of=${LNX_dev}
+}
+
+sync(){
+  rsync \
+    -aAX \
+    --delete \
+    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} \
+    "/" "${LTMPDIR}"
+}
+
+mparts(){
+  pdev=$1
+  echo "[I] mounting partitions of [$pdev] on ${LTMPDIR}"
+
+  mkdir -p ${LTMPDIR}
+  mount ${pdev}5 ${LTMPDIR}
+  
+  if [ -d ${LTMPDIR}/boot ]; then
+    echo "[I] boot/ found in root/"
+  else
+    # mainly for testing 
+    mkdir ${LTMPDIR}/boot
+    echo "[W] boot/ not found in root/"
+  fi
+
+  mount ${pdev}1 ${LTMPDIR}/boot
+}
+umparts(){
+  pdev=$1
+  echo "[I] unmounting partitions of [$pdev] on ${LTMPDIR}"
+
+  umount ${pdev}1 
+  umount ${pdev}5 
+}
+
+#dpart "${LNX_dev}" && sleep 1
+
+#dformat "${LNX_dev}" && sleep 1
+
+mparts "${LNX_dev}" && sleep 1
+
+#sync && sleep 1
+
+install_extlinux && sleep 1
+
+umparts "${LNX_dev}" && sleep 1
+
+
