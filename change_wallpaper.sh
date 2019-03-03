fe921c631cc4a7608a16a2453c2de8cab959a0ef
Jaya Balan A <bucket.size@gmail.com>
2019-03-03 11:12:39 +0530

    more scripts

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,8 +1,22 @@
- #! /usr/bin/env sh
- WALLPAPERS="$HOME/x-wallpaper"
+#! /usr/bin/env sh
+WALLPAPERS="$HOME/x-wallpaper"
 
- echo "WALLPAPERS=$WALLPAPERS"
- find -type f $WALLPAPERS | wc -l
+rndn=$(od -A n -t d -N 1 /dev/urandom)
 
- desktop_bg=$(find "$WALLPAPERS/" -type f | shuf | head -n 1) &&
- exec feh --bg-scale "$desktop_bg"
+idx=$((1 + rndn % 2000))
+url="https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1"
+xml=$(curl "$url" -s)
+ipath=$(xidel "$xml" -e "//image/url" -s)
+iname=$(echo $ipath | sed "s/\//\./g")
+wname="$WALLPAPERS/$iname"
+
+echo $idx
+echo $url
+echo $ipath
+echo $iname
+
+wget "http://www.bing.com/$ipath" -O "$wname" &&
+	exec feh --bg-scale "$wname"
+
+#desktop_bg=$(find "$WALLPAPERS/" -type f | shuf | head -n 1) &&
+#	exec feh --bg-scale "$wname"
---
+++ flash_nodemcu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+esptool.py --port /dev/ttyUSB0 write_flash -fm dio 0x00000 $1
