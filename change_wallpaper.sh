1e5ceaac752daef2684fb8654e8250e0d0a3ee50
Jaya Balan A <bucket.size@gmail.com>
2019-03-07 10:24:28 +0530

    wallpaper fail scenario

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,22 +1,23 @@
 #! /usr/bin/env sh
 WALLPAPERS="$HOME/x-wallpaper"
 
-rndn=$(od -A n -t d -N 1 /dev/urandom)
+if [ -f $WALLPAPERS ]; then
+    echo "$WALLPAPERS missing, creating it"
+    mkdir -p $WALLPAPERS
+fi
 
+rndn=$(od -A n -t d -N 1 /dev/urandom)
 idx=$((1 + rndn % 2000))
-url="https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1"
-xml=$(curl "$url" -s)
-ipath=$(xidel "$xml" -e "//image/url" -s)
+url="https://www.bing.com/HPImageArchive.aspx?format=xml&idx=$idx&n=1"
+
+ipath=$(curl -s "$url" | xmllint --xpath "//image/url/text()" -)
 iname=$(echo $ipath | sed "s/\//\./g")
 wname="$WALLPAPERS/$iname"
-
-echo $idx
-echo $url
-echo $ipath
-echo $iname
 
 wget "http://www.bing.com/$ipath" -O "$wname" &&
 	exec feh --bg-scale "$wname"
 
-#desktop_bg=$(find "$WALLPAPERS/" -type f | shuf | head -n 1) &&
-#	exec feh --bg-scale "$wname"
+if [ "$" != "0" ]; then
+    wname=$(find "$WALLPAPERS/" -type f | shuf | head -n 1) &&
+        exec feh --bg-scale "$wname"
+fi
