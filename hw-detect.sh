cebeddbad2c53e2cbeaefc1c6cbb2fb22774a2e6
Bucket Size <bucket.size@gmail.com>
2020-01-30 17:14:51 +0530

    tint2 cleanup

--- hw-detect.sh
+++ hw-detect.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 case $1 in
-    lan)
+    elan)
         dev=$(ip link | grep -Po '(?<=[0-9]: )(enp.*)(?=: )')
         ;;
     wlan)
         dev=$(ip link | grep -Po '(?<=[0-9]: )(wlp.*)(?=: )')
+        ;;
+    bat-cap)
+        dev=$(acpi -bi | grep -Po '(?<=ing, )(.*?)(?=%)')
+        ;;
+    bat-full-cap)
+        dev=$(acpi -bi | grep -Po '(?<=last full capacity )(.*?)(?= mAh )')
+        ;;
+    bat-design-cap)
+        dev=$(acpi -bi | grep -Po '(?<=design capacity )(.*?)(?= mAh)')
         ;;
 esac
 
