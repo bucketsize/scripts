f99602939494a9de19abbe2e864cc90dfadf6a3f
Bucket Size <bucket.size@gmail.com>
2020-01-31 15:15:35 +0530

    using /sys fs to set brightness

--- x.brightness.sh
+++ x.brightness.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
+# TODO: handle multiple connected devices
 
-if [ -z "$1"]; then
-    br=0.5
-else
-    br=$1
+blp=/sys/class/backlight
+bld=$(ls $blp)
+bs=$(cat $blp/$bld/brightness)
+bn=$(echo "($bs+0.5)/1" | bc)
+bx=0
+
+case $1 in
+    down)
+        if [ $bn -gt 10 ]; then
+            bx=$(echo "($bn-10)" | bc)
+        fi
+        ;;
+    up)
+        if [ $bn -lt 100 ]; then
+            bx=$(echo "($bn+10)" | bc)
+        fi
+        ;;
+esac
+if [ $bx -gt 0 ]; then
+    echo $bx | sudo tee $blp/$bld/brightness
 fi
-dev=`xrandr -q | grep " connected" | cut -f 1 -d " "`
-xrandr --output ${dev} --brightness ${br}
