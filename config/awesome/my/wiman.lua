d19e5bedde0a1092111af8fe27d026ce4eb94926
JB <bucket.size@gmail.com>
2020-02-15 20:22:29 +0530

    widgets management componentized

--- config/awesome/my/wiman.lua
+++ config/awesome/my/wiman.lua
@@ -3,226 +3,221 @@ local wibox = require("wibox")
 local lain  = require("lain")
 local markup = lain.util.markup
 
-local theme                                     = {}
-theme.font                                      = "DejaVu Sans 10"
-theme.font_mono                                 = "DejaVu Sans Mono 10"
-theme.fg_normal                                 = "#FEFEFE"
-theme.fg_focus                                  = "#32D6FF"
-theme.fg_urgent                                 = "#C83F11"
-theme.bg_normal                                 = "#222222"
+function objdump(tag, o)
+	print(tag .. ':')
+	if type(o) == 'table' then
+		for k,v in pairs(o) do
+			print('\t' .. k .. ' = ' .. tostring(v))
+		end
+	else
+		print('\t' .. tostring(o))
+	end
+end
 
-local	sprtr = wibox.widget.textbox()
-sprtr:set_text(" ")
+Wiman = {}
+function Wiman:new(theme, screen)
+	self.theme = theme
+	self.screen = screen
+	return self
+end
 
-local co_widgets = {}
-co_widgets.kb = awful.widget.keyboardlayout()
-co_widgets.clock = wibox.widget.textclock()
+function Wiman:build()
+	local	sprtr = wibox.widget.textbox()
+	sprtr:set_text(" | ")
 
-local lain_widgets = {}
+	local wgts = {}
+	wgts.kb = awful.widget.keyboardlayout()
+	wgts.clock = wibox.widget.textclock()
 
----- Calendar
-lain_widgets.cal = lain.widget.cal({
-    attach_to = { co_widgets.clock },
-    notification_preset = {
-        font = theme.font_mono,
-        fg   = theme.fg_normal,
-        bg   = theme.bg_normal
-    }
-})
+	local theme = self.theme
 
----- Mail IMAP check
---lain_widgets.mailicon = wibox.widget.imagebox(theme.widget_mail)
-----[[ commented because it needs to be set before use
---lain_widgets.mailicon:buttons(my_table.join(awful.button({ }, 1, function () awful.spawn(mail) end)))
---theme.mail = lain.widget.imap({
-    --timeout  = 180,
-    --server   = "server",
-    --mail     = "mail",
-    --password = "keyring get mail",
-    --settings = function()
-        --if mailcount > 0 then
-            --widget:set_markup(markup.font(theme.font, " " .. mailcount .. " "))
-            --mailicon:set_image(theme.widget_mail_on)
-        --else
-            --widget:set_text("")
-            --mailicon:set_image(theme.widget_mail)
-        --end
-    --end
---})
-----]]
+	---- Calendar
+	wgts.cal = lain.widget.cal({
+			attach_to = { wgts.clock },
+			notification_preset = {
+				font = theme.font_mono,
+				fg   = theme.fg_normal,
+				bg   = theme.bg_normal
+			}
+		})
 
----- MPD
---lain_widgets.musicplr = awful.util.terminal .. " -title Music -g 130x34-320+16 -e ncmpcpp"
---lain_widgets.mpdicon = wibox.widget.imagebox(theme.widget_music)
---lain_widgets.mpdicon:buttons(my_table.join(
-    --awful.button({ modkey }, 1, function () awful.spawn.with_shell(musicplr) end),
-    --awful.button({ }, 1, function ()
-        --os.execute("mpc prev")
-        --theme.mpd.update()
-    --end),
-    --awful.button({ }, 2, function ()
-        --os.execute("mpc toggle")
-        --theme.mpd.update()
-    --end),
-    --awful.button({ }, 3, function ()
-        --os.execute("mpc next")
-        --theme.mpd.update()
-    --end)))
---theme.mpd = lain.widget.mpd({
-    --settings = function()
-        --if mpd_now.state == "play" then
-            --artist = " " .. mpd_now.artist .. " "
-            --title  = mpd_now.title  .. " "
-            --mpdicon:set_image(theme.widget_music_on)
-        --elseif mpd_now.state == "pause" then
-            --artist = " mpd "
-            --title  = "paused "
-        --else
-            --artist = ""
-            --title  = ""
-            --mpdicon:set_image(theme.widget_music)
-        --end
+	---- Mail IMAP check
+	--wgts.mailicon = wibox.widget.imagebox(theme.widget_mail)
+	----[[ commented because it needs to be set before use
+	--wgts.mailicon:buttons(my_table.join(awful.button({ }, 1, function () awful.spawn(mail) end)))
+	--theme.mail = lain.widget.imap({
+	--timeout  = 180,
+	--server   = "server",
+	--mail     = "mail",
+	--password = "keyring get mail",
+	--settings = function()
+	--if mailcount > 0 then
+	--widget:set_markup(markup.font(theme.font, " " .. mailcount .. " "))
+	--mailicon:set_image(theme.widget_mail_on)
+	--else
+	--widget:set_text("")
+	--mailicon:set_image(theme.widget_mail)
+	--end
+	--end
+	--})
+	----]]
 
-        --widget:set_markup(markup.font(theme.font, markup("#EA6F81", artist) .. title))
-    --end
---})
+	---- MPD
+	--wgts.musicplr = awful.util.terminal .. " -title Music -g 130x34-320+16 -e ncmpcpp"
+	--wgts.mpdicon = wibox.widget.imagebox(theme.widget_music)
+	--wgts.mpdicon:buttons(my_table.join(
+	--awful.button({ modkey }, 1, function () awful.spawn.with_shell(musicplr) end),
+	--awful.button({ }, 1, function ()
+	--os.execute("mpc prev")
+	--theme.mpd.update()
+	--end),
+	--awful.button({ }, 2, function ()
+	--os.execute("mpc toggle")
+	--theme.mpd.update()
+	--end),
+	--awful.button({ }, 3, function ()
+	--os.execute("mpc next")
+	--theme.mpd.update()
+	--end)))
+	--theme.mpd = lain.widget.mpd({
+	--settings = function()
+	--if mpd_now.state == "play" then
+	--artist = " " .. mpd_now.artist .. " "
+	--title  = mpd_now.title  .. " "
+	--mpdicon:set_image(theme.widget_music_on)
+	--elseif mpd_now.state == "pause" then
+	--artist = " mpd "
+	--title  = "paused "
+	--else
+	--artist = ""
+	--title  = ""
+	--mpdicon:set_image(theme.widget_music)
+	--end
 
----- MEM
-lain_widgets.mem = lain.widget.mem({
-    settings = function()
-        widget:set_markup(markup.font(theme.font, "mem: " .. mem_now.used .. "MB "))
-    end
-})
+	--widget:set_markup(markup.font(theme.font, markup("#EA6F81", artist) .. title))
+	--end
+	--})
 
----- CPU
-lain_widgets.cpu = lain.widget.cpu({
-    settings = function()
-        widget:set_markup(markup.font(theme.font, "cpu: " .. cpu_now.usage .. "% "))
-    end
-})
-
----- Coretemp
-lain_widgets.temp = lain.widget.temp({
-    settings = function()
-        widget:set_markup(markup.font(theme.font, "T: " .. coretemp_now .. "°C "))
-    end
-})
-
----- / fs
-lain_widgets.fs = lain.widget.fs({
-    notification_preset = { fg = theme.fg_normal, bg = theme.bg_normal, font = theme.font_mono_10 },
-    settings = function()
-        widget:set_markup(markup.font(theme.font_mono, " " .. fs_now["/"].percentage .. "% "))
-    end
-})
-
----- Battery
-lain_widgets.bat = lain.widget.bat({
-    settings = function()
-        if bat_now.status and bat_now.status ~= "N/A" then
-            if bat_now.ac_status == 1 then
-                baticon:set_image(theme.widget_ac)
-            elseif not bat_now.perc and tonumber(bat_now.perc) <= 5 then
-                baticon:set_image(theme.widget_battery_empty)
-            elseif not bat_now.perc and tonumber(bat_now.perc) <= 15 then
-                baticon:set_image(theme.widget_battery_low)
-            else
-                baticon:set_image(theme.widget_battery)
-            end
-            widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% "))
-        else
-            widget:set_markup(markup.font(theme.font, " AC "))
-        end
-    end
-})
-
----- ALSA volume
-lain_widgets.vol = lain.widget.alsa({
-		settings = function()
-			if volume_now.status == "off" then
-			elseif tonumber(volume_now.level) == 0 then
-			elseif tonumber(volume_now.level) <= 50 then
-			else
+	---- MEM
+	wgts.mem = lain.widget.mem({
+			settings = function()
+				widget:set_markup(markup.font(theme.font, string.format("mem: %02i%s", mem_now.usage, "%")))
 			end
-			widget:set_markup(markup.font(theme.font, " " .. volume_now.level .. "% "))
-		end
-	})
-lain_widgets.vol.widget:buttons(awful.util.table.join(
-		awful.button({}, 4, function ()
-			awful.util.spawn("amixer set Master 5%+")
-			theme.volume.update()
-		end),
-		awful.button({}, 5, function ()
-			awful.util.spawn("amixer set Master 5%-")
-			theme.volume.update()
-		end)
-	))
+		})
 
----- Net
-lain_widgets.net = lain.widget.net({
-		settings = function()
-			widget:set_markup(markup.font(theme.font,
-					markup("#7AC82E", " " .. string.format("%06.1f", net_now.received))
-					.. " " ..
-				markup("#46A8C3", " " .. string.format("%06.1f", net_now.sent) .. " ")))
-		end
-	})
+	---- CPU
+	wgts.cpu = lain.widget.cpu({
+			settings = function()
+				widget:set_markup(markup.font(theme.font, string.format("cpu: %02i%s", cpu_now.usage, "%")))
+			end
+		})
 
----- Weather
--- theme.lain_widgets.weather = lain.widget.weather({
--- 		city_id = 1277333, -- placeholder (London)
--- 		notification_preset = { font = theme.font_mono_10, fg = theme.fg_normal },
--- 		weather_na_markup = markup.fontfg(theme.font, theme.fg_normal, "N/A "),
--- 		settings = function()
--- 			descr = weather_now["weather"][1]["description"]:lower()
--- 			units = math.floor(weather_now["main"]["temp"])
--- 			widget:set_markup(markup.fontfg(theme.font, theme.fg_normal, descr .. " @ " .. units .. "°C "))
--- 		end
--- 	})
+	---- Coretemp
+	wgts.temp = lain.widget.temp({
+			settings = function()
+				widget:set_markup(markup.font(theme.font, string.format("T: %s°C", coretemp_now )))
+			end
+		})
 
-		local my_widgets = {}
-		my_widgets.vol = lain_widgets.vol
-		my_widgets.cpu = lain_widgets.cpu
-		my_widgets.mem = lain_widgets.mem
-		my_widgets.temp = lain_widgets.temp
-		my_widgets.bat= lain_widgets.bat
-		my_widgets.net= lain_widgets.net
-		my_widgets.fs= lain_widgets.fs
-		my_widgets.cal= lain_widgets.cal
+	---- / fs
+	wgts.fs = lain.widget.fs({
+			notification_preset = { fg = theme.fg_normal, bg = theme.bg_normal, font = theme.font_mono },
+			settings = function()
+				widget:set_markup(markup.font(theme.font_mono, string.format("/: %02i%s",fs_now["/"].percentage, "%")))
+			end
+		})
 
-		co_widgets.clock:connect_signal("button::press",
-			function(_, _, _, button)
-				if button == 1 then my_widgets.cal.toggle() end
+	---- Battery
+	wgts.bat = lain.widget.bat({
+			settings = function()
+				if bat_now.status and bat_now.status ~= "N/A" then
+					if bat_now.ac_status == 1 then
+						baticon:set_image(theme.widget_ac)
+					elseif not bat_now.perc and tonumber(bat_now.perc) <= 5 then
+						baticon:set_image(theme.widget_battery_empty)
+					elseif not bat_now.perc and tonumber(bat_now.perc) <= 15 then
+						baticon:set_image(theme.widget_battery_low)
+					else
+						baticon:set_image(theme.widget_battery)
+					end
+					widget:set_markup(markup.font(theme.font, string.format("bat: %02i%s", bat_now.perc, "% ")))
+				else
+					widget:set_markup(markup.font(theme.font, " AC "))
+				end
+			end
+		})
+
+	---- ALSA volume
+	wgts.vol = lain.widget.alsa({
+			settings = function()
+				if volume_now.status == "off" then
+				elseif tonumber(volume_now.level) == 0 then
+				elseif tonumber(volume_now.level) <= 50 then
+				else
+				end
+				widget:set_markup(markup.font(theme.font, string.format("vol: %02i%s", volume_now.level, "%")))
+			end
+		})
+	wgts.vol.widget:buttons(awful.util.table.join(
+			awful.button({}, 4, function ()
+				awful.util.spawn("amixer set Master 5%+")
+				wgts.vol.update()
+			end),
+			awful.button({}, 5, function ()
+				awful.util.spawn("amixer set Master 5%-")
+				wgts.vol.update()
 			end)
+		))
 
-local wiman = {
-	boxes_right = function(ctx)
-		local s = ctx.screen
-		return
+	---- Net
+	wgts.net = lain.widget.net({
+			settings = function()
+				widget:set_markup(markup.font(theme.font,
+						markup("#7AC82E", " " .. string.format("%06.1f", net_now.received))
+						.. " " ..
+					markup("#46A8C3", " " .. string.format("%06.1f", net_now.sent) .. " ")))
+			end
+		})
+
+	---- Weather
+	wgts.weather = lain.widget.weather({
+			city_id = 1277333, -- placeholder (London)
+			notification_preset = { font = theme.font_mono, fg = theme.fg_normal },
+			weather_na_markup = markup.fontfg(theme.font, theme.fg_normal, "N/A "),
+			settings = function()
+				descr = weather_now["weather"][1]["description"]:lower()
+				units = math.floor(weather_now["main"]["temp"])
+				widget:set_markup(markup.fontfg(theme.font, theme.fg_normal, descr .. " @ " .. units .. "°C "))
+			end
+		})
+
+	wgts.clock:connect_signal("button::press",
+		function(_, _, _, button)
+			if button == 1 then wgts.cal.toggle() end
+		end)
+
+		self.widgets = wgts
+		objdump('self', self)
+	end
+
+function Wiman:boxes_right()
+	local s	= self.screen
+	return
         { -- Right widgets
 					layout = wibox.layout.fixed.horizontal,
-					co_widgets.kb,
-					sprtr,
-					wibox.widget.systray(),
-					sprtr,
-					my_widgets.vol,
-					sprtr,
-					my_widgets.mem,
-					sprtr,
-					my_widgets.cpu,
-					sprtr,
-					my_widgets.bat,
-					sprtr,
-					my_widgets.net,
-					sprtr,
-					my_widgets.fs,
-					sprtr,
-					--my_widgets.weather,
-					co_widgets.clock,
-					sprtr,
-					s.mylayoutbox,
+					self.widgets.kb,
+										self.widgets.weather,
+										wibox.widget.systray(),
+										self.widgets.vol, sprtr,
+										self.widgets.mem,
+										self.widgets.cpu,
+										self.widgets.temp,
+										self.widgets.bat,
+										self.widgets.net,
+										self.widgets.fs,
+										self.widgets.clock,
+										s.mylayoutbox,
         }
 	end
-}
 
-return wiman
+return Wiman
