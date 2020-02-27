9e68e49c92146ed386dc7035e32bfb2206cb3919
JB <bucket.size@gmail.com>
2020-02-27 10:24:27 +0530

    modularized awesome rc further

--- config/awesome/my/wiman.lua
+++ config/awesome/my/wiman.lua
@@ -63,6 +63,7 @@ end
 Wiman = {}
 function Wiman:setup(ctx)
 	self.ctx = ctx
+	self:build()
 	return self
 end
 
@@ -261,7 +262,6 @@ function Wiman:build()
 	end
 
 	function Wiman:boxes_right()
-		local s	= self.ctx.screen
 		return
 		{ -- Right widgets
 			layout = wibox.layout.fixed.horizontal,
@@ -277,8 +277,114 @@ function Wiman:build()
 			self.widgets.weather,
 			self.widgets.clock,
 			self.widgets.kb,
-			s.mylayoutbox,
+			self.mylayoutbox,
 		}
 	end
 
+	function Wiman:apply_wallpaper_in_screen(s)
+		local beautiful = self.ctx.beautiful
+		-- Wallpaper
+		if beautiful.wallpaper then
+			local wallpaper = beautiful.wallpaper
+			-- If wallpaper is a function, call it with the screen
+			if type(wallpaper) == "function" then
+				wallpaper = wallpaper(s)
+			end
+			gears.wallpaper.maximized(wallpaper, s, true)
+		end
+	end
+	function Wiman:apply()
+		-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+		screen.connect_signal("property::geometry", function(s)
+			self:apply_wallpaper_in_screen(s)
+		end)
+
+		-- Setup screen and stuff
+		awful.screen.connect_for_each_screen(function(s)
+			self:apply_in_screen(s)
+		end)
+	end
+	function Wiman:apply_in_screen(s)
+		self:apply_wallpaper_in_screen(s)
+
+		-- Create a wibox for each screen and add it
+		local taglist_buttons = gears.table.join(
+			awful.button({ }, 1, function(t) t:view_only() end),
+			awful.button({ modkey }, 1, function(t)
+				if client.focus then
+					client.focus:move_to_tag(t)
+				end
+			end),
+			awful.button({ }, 3, awful.tag.viewtoggle),
+			awful.button({ modkey }, 3, function(t)
+				if client.focus then
+					client.focus:toggle_tag(t)
+				end
+			end),
+			awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+			awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+			)
+		local tasklist_buttons = gears.table.join(
+			awful.button({ }, 1, function (c)
+				if c == client.focus then
+					c.minimized = true
+				else
+					c:emit_signal(
+						"request::activate",
+						"tasklist",
+						{raise = true}
+						)
+				end
+			end),
+			awful.button({ }, 3, function()
+				awful.menu.client_list({ theme = { width = 250 } })
+			end),
+			awful.button({ }, 4, function ()
+				awful.client.focus.byidx(1)
+			end),
+			awful.button({ }, 5, function ()
+				awful.client.focus.byidx(-1)
+			end))
+			-- Each screen has its own tag table.
+			awful.tag({ "1", "2", "3", "4" }, s, awful.layout.layouts[1])
+
+			-- Create a promptbox for each screen
+			self.mypromptbox = awful.widget.prompt()
+			-- Create an imagebox widget which will contain an icon indicating which layout we're using.
+			-- We need one layoutbox per screen.
+			self.mylayoutbox = awful.widget.layoutbox(s)
+			self.mylayoutbox:buttons(gears.table.join(
+					awful.button({ }, 1, function () awful.layout.inc( 1) end),
+					awful.button({ }, 3, function () awful.layout.inc(-1) end),
+					awful.button({ }, 4, function () awful.layout.inc( 1) end),
+				awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+
+				-- Create a taglist widget
+				self.mytaglist = awful.widget.taglist {
+					screen  = s,
+					filter  = awful.widget.taglist.filter.all,
+					buttons = taglist_buttons
+				}
+
+				-- Create a tasklist widget
+				self.mytasklist = awful.widget.tasklist {
+					screen  = s,
+					filter  = awful.widget.tasklist.filter.currenttags,
+					buttons = tasklist_buttons
+				}
+				-- Create the wibox
+				self.mywibox = awful.wibar({ position = "top", screen = s })
+
+				-- Add widgets to the wibox
+				self.mywibox:setup {
+					layout = wibox.layout.align.horizontal,
+					{ -- Left widgets
+						layout = wibox.layout.fixed.horizontal,
+						self.mytaglist,
+						self.mypromptbox,
+					},
+					self.mytasklist, -- Middle widget
+					self:boxes_right()
+				}
+			end
 	return Wiman
