56fc990af8f87de6249a43b1b6086305fd14dee8
Bucket Size <bucket.size@gmail.com>
2020-03-25 22:15:54 +0530

    visual fixes

--- config/openbox/setup.sh
+++ config/openbox/setup.sh
@@ -6,12 +6,10 @@ rm ~/.config/openbox
 rm ~/.config/tint2
 rm ~/.config/conky
 rm ~/.config/dunst
-rm ~/.config/sxhkd
 rm ~/.config/compton.conf
-rm ~/.config/awesome
 
 ln -s ~/scripts/config/openbox/my ~/.config/openbox
 ln -s ~/scripts/config/tint2/my-v ~/.config/tint2
-ln -s ~/scripts/config/conky/my ~/.config/conky
+ln -s ~/scripts/config/conky/asuramin ~/.config/conky
 ln -s ~/scripts/config/dunst/my ~/.config/dunst
 ln -s ~/scripts/config/compton/my/compton.conf ~/.config/
