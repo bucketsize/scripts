fc024bb8f29cce189ec2540dc21103acc97e70ed
JB <bucket.size@gmail.com>
2020-08-21 19:27:36 +0530

    cleaned up openbox

--- config/openbox/setup.sh
+++ config/openbox/setup.sh
@@ -7,10 +7,12 @@ rm ~/.config/tint2
 rm ~/.config/conky
 rm ~/.config/dunst
 rm ~/.config/compton.conf
+rm ~/.Xresources
 
 ln -s ~/scripts/config/openbox/my ~/.config/openbox
 ln -s ~/scripts/config/tint2/my-v ~/.config/tint2
-ln -s ~/scripts/config/conky/asuramin ~/.config/conky
+ln -s ~/scripts/config/conky/simple ~/.config/conky
 ln -s ~/scripts/config/dunst/my ~/.config/dunst
+ln -s ~/scripts/config/sxhkd/my ~/.config/sxhkd
 ln -s ~/scripts/config/compton/default/compton.conf ~/.config/
 ln -s ~/scripts/config/Xresources ~/.Xresources
