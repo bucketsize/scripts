65c9dc6340c2e8879d40c1f00c8e6c3be7266e89
JB <bucket.size@gmail.com>
2020-12-09 15:35:08 +0530

    shortcuts, beautify

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -1,12 +1,15 @@
 import XMonad
 import XMonad.Util.Paste
 import XMonad.Util.EZConfig
+import XMonad.Util.SpawnOnce
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Config.Desktop
 import XMonad.Actions.WindowBringer
+import XMonad.Actions.GridSelect
 
+import Control.Exception
 import Control.Monad
 import Data.Monoid
 import System.IO
@@ -15,12 +18,24 @@ import System.Exit
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 
+handleShutdown :: AsyncException -> IO ()
+handleShutdown e = do
+  forM_ oAutostart $ \(s, k) ->
+    spawn k
+  throw e
+
+handleStartup = do
+  forM_ oAutostart $ \(s, k) ->
+    putStrLn ("start " ++ s) >> spawn k
+  forM_ oAutostart $ \(s, k) ->
+    putStrLn ("kill " ++ k)  >> spawn s
+
 main = do
-  forM_ oAutostart $ \s ->
-    spawn s
+  catch start handleShutdown
 
+start = do
+  handleStartup
   xmproc <- spawnPipe "xmobar ~/scripts/config/xmobar/xmobarrc"
-
   xmonad $ docks desktopConfig
     { terminal          = "sakura"
     , focusFollowsMouse = True
@@ -37,7 +52,7 @@ main = do
     , manageHook        = oManageHook
     -- handleEventHook = myEventHook
     -- logHook = myLogHook
-    -- startupHook = myStartupHook
+    -- , startupHook       = handleStartup
     , logHook           = dynamicLogWithPP xmobarPP
         { ppOutput = hPutStrLn xmproc
         , ppTitle  = xmobarColor "green" "" . shorten 50
@@ -46,6 +61,8 @@ main = do
       `additionalKeysP` oAddlKeysP
       `additionalKeys`oAddlKeys
 
+dmenuArgs = ["-l", "10"]
+
 oAddlKeysP =
   [ ("C-]",                    spawn vol_up)
   , ("C-[",                    spawn vol_down)
@@ -53,6 +70,7 @@ oAddlKeysP =
   , ("<XF86AudioRaiseVolume>", spawn vol_up)
   , ("<XF86AudioLowerVolume>", spawn vol_down)
   , ("<XF86AudioMute>",        spawn vol_mute)
+  , ("M-w",                    goToSelected defaultGSConfig)
   ]
 
 oAddlKeys =
@@ -62,7 +80,7 @@ oAddlKeys =
   , ((mod4Mask,                 xK_r),      spawn "dmenu_run -l 10")
   , ((mod4Mask,                 xK_q),      spawn "xmonad --recompile && xmonad --restart")
   , ((mod4Mask,                 xK_l),      spawn scr_lock)
-  , ((mod4Mask,                 xK_w),      gotoMenu)
+  , ((mod4Mask,                 xK_w),      gotoMenuArgs dmenuArgs)
   , ((mod4Mask,                 xK_b),      bringMenu)
   , ((controlMask .|. mod1Mask, xK_1),      spawn dtop_viga)
   , ((controlMask .|. mod1Mask, xK_2),      spawn dtop_hdmi)
@@ -89,19 +107,39 @@ oManageHook = composeAll
 
 -- startup
 oAutostart =
-  [ "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 20;"
-  , "nm-applet --sm-disable;"
-  , "sleep 2; ~/scripts/sys_ctl/ctl.lua fun pa_set_default;"
-  , "sleep 2; ~/scripts/sys_ctl/ctl.lua cmd dtop_viga;"
-  , "sleep 2; dunst;"
-  , "sleep 1; light-locker --lock-on-suspend --lock-on-lid;"
-  , "sleep 1; xautolock"
+  [ ( "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 20;"
+    , "killall trayer"
+    )
+  , ( "nm-applet --sm-disable;"
+    , "killall nm-applet"
+    )
+  , ( "sleep 2; ~/scripts/sys_ctl/ctl.lua fun pa_set_default;"
+    , ""
+    )
+  , ( "sleep 2; ~/scripts/sys_ctl/ctl.lua cmd dtop_viga;"
+    , ""
+    )
+  , ( "sleep 2; dunst;"
+    , "killall dunst"
+    )
+  , ( "sleep 1; light-locker --lock-on-suspend --lock-on-lid;"
+    , "killall light-locker"
+    )
+  , ( "sleep 1; xautolock"
       ++ " -time 3 -locker \"~/scripts/sys_ctl/ctl.lua fun scr_lock_if\""
       ++ " -killtime 10 -killer \"notify-send -u critical -t 10000 -- 'Killing system ...'\""
       ++ " -notify 30 -notifier \"notify-send -u critical -t 10000 -- 'Locking system ETA 30s ...'\";"
-  , "sleep 1; picom -cb;"
-  , "sleep 2; ~/scripts/sys_mon/daemon.lua;"
-  , "sleep 3; ~/scripts/xdg/x.wallpaper.sh cycle;"
+    , "killall xautolock"
+    )
+  , ( "sleep 1; picom -cb;"
+    , "killall picom"
+    )
+  , ( "sleep 2; ~/scripts/sys_mon/daemon.lua;"
+    , "pkill -f daemon.lua"
+    )
+  , ( "sleep 3; ~/scripts/xdg/x.wallpaper.sh cycle;"
+    , ""
+    )
   ]
 
 -- commands
@@ -122,14 +160,14 @@ kb_led_off  = "xset led off"
 pref_mode   = "1280x720"
 
 dtop_viga   = "xrandr"
-    ++ " --output DisplayPort-0 --mode 1280x720 --pos 0x0 --rotate normal"
-    ++ " --output HDMI-A-0 --off"
-    ++ " --output DVI-D-0 --off"
+              ++ " --output DisplayPort-0 --mode 1280x720 --pos 0x0 --rotate normal"
+              ++ " --output HDMI-A-0 --off"
+              ++ " --output DVI-D-0 --off"
 dtop_hdmi   = "xrandr"
-    ++ " --output DisplayPort-0 --off"
-    ++ " --output HDMI-A-0 --mode 1280x720 --pos 1280x0 --rotate normal"
-    ++ " --output DVI-D-0 --off"
+              ++ " --output DisplayPort-0 --off"
+              ++ " --output HDMI-A-0 --mode 1280x720 --pos 1280x0 --rotate normal"
+              ++ " --output DVI-D-0 --off"
 dtop_extn   = "xrandr"
-    ++ " --output DisplayPort-0 --mode 1280x720 --pos 0x0 --rotate normal --primary"
-    ++ " --output HDMI-A-0 --mode 1280x720 --pos 1280x0 --rotate normal"
-    ++ " --output DVI-D-0 --off"
+              ++ " --output DisplayPort-0 --mode 1280x720 --pos 0x0 --rotate normal --primary"
+              ++ " --output HDMI-A-0 --mode 1280x720 --pos 1280x0 --rotate normal"
+              ++ " --output DVI-D-0 --off"
