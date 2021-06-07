4bc31816e1c0f777a026f49d68284ddf0520a6cd
JB <bucket.size@gmail.com>
2021-06-07 10:09:55 +0530

    xresources, emacs

--- config/emacs.d/.custom.el
+++ config/emacs.d/.custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (xclip which-key wgrep web-mode use-package spacemacs-theme smex smartparens racket-mode pyenv-mode py-autopep8 pip-requirements org-projectile org-bullets neotree magit-popup magit js2-refactor js-comint intero impatient-mode hlinum go-eldoc ggtags flymake-go expand-region exec-path-from-shell evil elpy dashboard counsel-projectile company-tern company-go company-c-headers avy))))
+   '(intero xclip which-key wgrep web-mode use-package undo-tree tern spacemacs-theme smex smartparens racket-mode pyenv-mode py-autopep8 pip-requirements org-projectile org-bullets magit-popup magit js2-refactor js-comint impatient-mode hlinum hindent haskell-mode go-eldoc ggtags flymake-go flycheck expand-region exec-path-from-shell evil elpy dashboard counsel-projectile company-go company-c-headers avy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
