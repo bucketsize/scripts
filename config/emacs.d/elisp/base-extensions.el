be576af573904c9e1a58e2f8833b4d86be59ff52
JB <bucket.size@gmail.com>
2021-06-14 00:47:26 +0530

    emacs, dunst config tweaks

--- config/emacs.d/elisp/base-extensions.el
+++ config/emacs.d/elisp/base-extensions.el
@@ -177,6 +177,20 @@
 ;; vim-mode configuration
 ;; https://github.com/emacs-evil/evil
 (use-package evil
-	:config (evil-mode 1))
+	:config 
+  (evil-mode 1)
+  (add-hook 'neotree-mode-hook
+              (lambda ()
+                (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+                (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+                (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+                (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+                (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+                (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+                (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+                (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))
+  )
+
 
 (provide 'base-extensions)
