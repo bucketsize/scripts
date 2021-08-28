c3506675b1fc5fb68d6d85e9525cbcb23d88efac
JB <bucket.size@gmail.com>
2021-08-28 22:08:14 +0530

    cleanup

--- config/awesome/my/config.lua
+++
@@ -1,9 +0,0 @@
-local Config = {
-	pam_domain = 'xdm',
-	terminal   = 'lxterminal',
-	webbrowser = 'firefox',
-	fsbrowser  = 'pcmanfm',
-	editor     = 'gvim',
-	term_editor= 'vim',
-}
-return Config
