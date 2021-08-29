1ee09d602571efa604d1df22deeaa207ce2b5518
JB <bucket.size@gmail.com>
2021-08-29 19:48:45 +0530

    fixes

--- config/autostart.sh
+++ config/autostart.sh
@@ -1,25 +1,37 @@
 #!/bin/sh
 
+# dbus for dbus aware apps integration . ie pulseaudio, nautilus, firefox, dunst
+dbus-launch --sh-syntax --exit-with-session
+
+sleep 1
+dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
+
 # local typed k,v store
-(sleep 1; ~/.luarocks/bin/frmad.cached - -) &
+~/.luarocks/bin/frmad.cached - - 2>&1 > /tmp/frmad.cached.log &
 
 # monitoring daemon
-export openweathermap_apikey=16704e3405a0cb1a1ae1b7917e4ff2fd
-(sleep 2; ~/.luarocks/bin/frmad.daemon) &
+sleep 1
+~/.luarocks/bin/frmad.daemon 2>&1 > /tmp/frmad.daemon.log &
 
-# dbus for dbus aware apps integration . ie pulseaudio, nautilus, firefox, dunst
-(sleep 1; dbus-launch --sh-syntax --exit-with-session)
-(sleep 2; dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY)
 
 # apply wallpaper after screen set
-(sleep 3; ~/.luarocks/bin/mxctl.control fun setup_video)
-(sleep 3; ~/scripts/xdg/x.wallpaper.sh cycle) &
+~/.luarocks/bin/mxctl.control fun setup_video
+~/scripts/xdg/x.wallpaper.sh cycle &
 
-(sleep 4; ~/.luarocks/bin/mxctl.control cmd autolockd_xautolock) &
-(sleep 5; picom) &
-(sleep 5; dunst) &
-(sleep 5; clipit) &
-(sleep 5; nm-applet --sm-disable) &
-(sleep 5; setxkbmap us) &
-(sleep 3; xsetroot -cursor_name left_ptr) &
-(sleep 5; VBoxClient --clipboard) &
+~/.luarocks/bin/mxctl.control cmd autolockd_xautolock &
+(if [ -f /usr/bin/picom ];
+then picom;
+else compton;
+	fi) &
+
+sleep 1
+dunst &
+clipit &
+nm-applet --sm-disable &
+setxkbmap us &
+xsetroot -cursor_name left_ptr &
+
+sleep 1
+(if [ -f /usr/bin/VBoxClient ];
+then VBoxClient --clipboard;
+	fi)	&
