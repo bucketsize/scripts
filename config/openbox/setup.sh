2a0d1895833910b643a4ff11f6027a2f059171fe
JB <bucket.size@gmail.com>
2021-09-28 15:54:54 +0530

    openbox default Tenebris theme

--- config/openbox/setup.sh
+++ config/openbox/setup.sh
@@ -1,3 +1,4 @@
 checkpkgs "openbox obconf"
 updatelink ~/scripts/config/openbox/my ~/.config/openbox
-
+[ -d ~/.themes] || mkdir ~/.themes
+ln -s ~/scripts/config/openbox/themes/Tenebris/ ~/.themes/
