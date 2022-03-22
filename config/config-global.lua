815d51b6781f6e5b36785afe25c0ed6130cd3898
Herr Bucket <bucket.size@gmail.com>
2022-03-23 01:13:13 +0530

    cleanup

---
+++ config/config-global.lua
@@ -0,0 +1,7 @@
+return {
+    __font_global           = "Cantarell 10",
+    __font_global_monospace = "Monospace 10",
+    __font_family_monospace = "Monospace",
+    __font_family_serif     = "Cantarell",
+    __font_family_sans      = "Cantarell"
+}
