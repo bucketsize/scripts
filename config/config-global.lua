f67c92503b8644b3b3120b398788ef8cefba89aa
Herr Bucket <bucket.size@gmail.com>
2022-04-01 17:26:35 +0530

    font fixes

--- config/config-global.lua
+++ config/config-global.lua
@@ -1,7 +1,7 @@
 return {
-    __font_global           = "Cantarell 10",
-    __font_global_monospace = "Monospace 10",
-    __font_family_monospace = "Monospace",
+    __font_global           = "Cantarell:size=10",
+    __font_global_monospace = "Monoid:size=10",
+    __font_family_monospace = "Monoid",
     __font_family_serif     = "Cantarell",
     __font_family_sans      = "Cantarell"
 }
