1fbce0fe9cf9521590b2159fee5ad90d5b185714
jb <jb@debugger2.net.loco>
2022-04-11 01:21:17 +0530

    alt-tab floating good

--- config/qtile/my/config.py
+++ config/qtile/my/config.py
@@ -25,40 +25,39 @@
 # SOFTWARE.
 
 from libqtile import bar, layout, widget, hook
+from libqtile import extension
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from re import findall
+from os import path
+from io import open
 
-mod = "mod4"
-alt = "mod1"
+mod        = "mod4"
+alt        = "mod1"
+terminal   = "qterminal"
+theme_file = "colors-terminal.sexy-4"
+
+def get_theme_from_file():
+    home = path.expanduser('~/')
+    theme_path = home + '.config/xresources.d/'+theme_file
+    f = open(theme_path, 'r')
+    theme = dict(
+        findall(r'[^!]\*?(\w*)\:\s*#?(.*)', f.read())
+    )
+    f.close()
+    if 'cursorColor' not in theme:
+        theme.update({ 'cursorColor': theme['color4']})
+    print(theme)
+    return theme
 
 # Colours
-color_theme = dict(
-    foreground='#CFCCD6',
-    background='#1B2021',
-    color0='#030405',
-    color8='#1f1c32',
-    color1='#8742a5',
-    color9='#9a5eb3',
-    color2='#406794',
-    color10='#5fd75f',
-    color3='#653c21',
-    color11='#6e9fcd',
-    color4='#8f4ff0',
-    color12='#BBC2E2',
-    color5='#5d479d',
-    color13='#998dd1',
-    color6='#3e3e73',
-    color14='#9a9dcc',
-    color7='#495068',
-    color15='#e1e1e4',
-)
-
-colors = [color_theme[f'color{n}'] for n in range(16)]
-color_alert = '#ee9900'
-color_frame = '#808080'
-
-terminal = guess_terminal()
+color_theme = get_theme_from_file()
+colors = ["black","red","green","yellow","blue","magenta","cyan","white"]
+colorv = [(color_theme[f'color{n}'], color_theme[f'color{n+8}']) for n in range(8)]
+colorm = dict(list(zip(colors, colorv)))
+color_alert = colorm["red"][1]
+color_frame = colorm["black"][0]
 
 @hook.subscribe.client_focus
 def float_to_front(w):
@@ -69,6 +68,28 @@ def autostart():
     home = os.path.expanduser('~')
     subprocess.Popen([home + '/.config/qtile/autostart'])
 
+@lazy.function
+def dmenu_run():
+    lazy.run_extension(extension.DmenuRun(
+        dmenu_prompt=">",
+        background="#15181a",
+        foreground="#00ff00",
+        selected_background="#079822",
+        selected_foreground="#fff",
+        dmenu_height=24,  # Only supported by some dmenu forks
+    ))
+
+@lazy.function
+def dmenu_windowlist():
+    lazy.run_extension(extension.WindowList(
+        dmenu_prompt=">",
+        all_groups = True,
+        background="#15181a",
+        foreground="#00ff00",
+        selected_background="#079822",
+        selected_foreground="#fff",
+        dmenu_height=24,  # Only supported by some dmenu forks
+    ))
 
 keys = [
     # A list of available commands that can be bound to keys can be found
@@ -79,19 +100,14 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    Key(
-        [alt], "grave",
-        lazy.window.bring_to_front()
-    ),
-    Key(
-        [alt], "Tab",
-        lazy.group.next_window()),
+
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
@@ -99,23 +115,52 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
     Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
     Key([mod], "f", lazy.window.toggle_floating(), desc='Toggle floating'),
-    Key(
-        [mod, alt], "f",
-        lazy.window.toggle_fullscreen()
-    ),
+    Key([mod, alt], "f", lazy.window.toggle_fullscreen()),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    
+    # ALT
+    Key([alt], "grave", lazy.window.bring_to_front()),
+    Key([alt], "Tab", lazy.group.next_window()),
+    Key([alt], 'r',  dmenu_run()),
+    Key([alt], 'w',  dmenu_windowlist()),
+
+    # Key(['mod4'], 'r', lazy.run_extension(extension.DmenuRun(
+    #     dmenu_prompt=">",
+    #     dmenu_font="terminus-8",
+    #     background="#15181a",
+    #     foreground="#00ff00",
+    #     selected_background="#079822",
+    #     selected_foreground="#fff",
+    #     dmenu_height=24,  # Only supported by some dmenu forks
+    # ))),
+
+
+    # Key([mod], 'm', lazy.run_extension(extension.CommandSet(
+    # commands={
+    #     'play/pause': '[ $(mocp -i | wc -l) -lt 2 ] && mocp -p || mocp -G',
+    #     'next': 'mocp -f',
+    #     'previous': 'mocp -r',
+    #     'quit': 'mocp -x',
+    #     'open': 'urxvt -e mocp',
+    #     'shuffle': 'mocp -t shuffle',
+    #     'repeat': 'mocp -t repeat',
+    #     },
+    # pre_commands=['[ $(mocp -i | wc -l) -lt 1 ] && mocp -S'],
+    # **Theme.dmenu))),
 ]
 
 groups = [Group(i) for i in "1234"]
@@ -156,106 +201,91 @@ widget_defaults = dict(
     fontsize=12,
     padding=3,
 )
+
 extension_defaults = widget_defaults.copy()
 
-class ThermalSensor(widget.ThermalSensor):
-	def poll(self):
-		temp_values = self.get_temp_sensors()
-		if temp_values is None:
-			return '---'
-		no = int(float(temp_values.get(self.tag_sensor, [0])[0]))
-		return '{}{}'.format(no, '°')#chr(0x1F321))
+top_bar = [
+    widget.Clock(
+        format='%a %d %b %Y %H:%M:%S',
+    ),
+    # widget.CurrentLayoutIcon(),
+    # widget.LaunchBar(
+    #     progs = [
+    #         ("Terminal", "qterminal", "Launch QTerminal")
+    #     ]
+    # ),
+    widget.WindowName(),
+    widget.Systray(),
+    widget.ThermalSensor(
+        update_interval=2
+    ),
+    widget.Volume(
+        update_interval=2
+    ),
+    # widget.CPUGraph(
+    #     graph_color=color_alert,
+    #     fill_color='{}.5'.format(color_alert),
+    #     border_color=color_frame,
+    #     line_width=2,
+    #     border_width=1,
+    #     samples=40,
+    #     width=50,
+    #     )
+    widget.CPU(
+        format='CPU {load_percent: 2.1f}%',
+        update_interval=2,
+    ),
+    # widget.MemoryGraph(
+    #     graph_color=color_alert,
+    #     fill_color='{}.5'.format(color_alert),
+    #     border_color=color_frame,
+    #     line_width=2,
+    #     border_width=1,
+    #     samples=40,
+    #     width=50,
+    #     ),
+    widget.Memory(
+        format="{MemUsed: .0f}{mm}",
+        update_interval=2,
+    ),
+    # widget.Net(
+    #    interface = "wlp0s20f3",
+    #    format = 'Net: {down} ↓↑ {up}',
+    #    padding = 5
+    #    ),
+    widget.Battery(energy_now_file = "charge_now",
+                   energy_full_file = "charge_full",
+                   power_now_file = "current_now",
+                   update_delay = 5,
+                   charge_char = u'↑',
+                   discharge_char = u'↓',),
+    widget.QuickExit(),
+]
 
-class Volume(widget.Volume):
-	def update(self):
-		vol = self.get_volume()
-		if vol != self.volume:
-			self.volume = vol
-			if vol < 0:
-				no = '0'
-			else:
-				no = int(vol / 100 * 9.999)
-			char = '♬'
-			self.text = '{}{}{}'.format(char, no, 'V')#chr(0x1F508))
-
-class Battery(widget.Battery):
-	def _get_text(self):
-		info = self._get_info()
-		if info is False:
-			return '---'
-		if info['full']:
-			no = int(info['now'] / info['full'] * 9.999)
-		else:
-			no = 0
-		if info['stat'] == 'Discharging':
-			char = self.discharge_char
-			if no < 2:
-				self.layout.colour = self.low_foreground
-			else:
-				self.layout.colour = self.foreground
-		elif info['stat'] == 'Charging':
-			char = self.charge_char
-		#elif info['stat'] == 'Unknown':
-		else:
-			char = '■'
-		return '{}{}{}'.format(char, no, 'B')#chr(0x1F506))
+bottom_bar = [
+    widget.GroupBox(),
+    widget.Prompt(),
+    widget.Chord(
+        chords_colors={
+            "launch": ("#ff0000", "#ffffff"),
+        },
+        name_transform=lambda name: name.upper(),
+    ),
+    widget.TaskList(
+        border=color_frame,
+        highlight_method='block',
+        max_title_width=800,
+        urgent_border=color_alert,
+    ),
+]
 
 screens = [
     Screen(
-        top=bar.Bar(
-            [
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                # widget.CurrentLayoutIcon(),
-                # widget.LaunchBar(
-                #     progs = [
-                #         ("Terminal", "qterminal", "Launch QTerminal")
-                #     ]
-                # ),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Systray(),
-                ThermalSensor(),
-                Volume(),
-                widget.CPUGraph(
-                    graph_color=color_alert,
-                    fill_color='{}.5'.format(color_alert),
-                    border_color=color_frame,
-                    line_width=2,
-                    border_width=1,
-                    samples=40,
-                    width=50,
-                    ),
-                widget.MemoryGraph(
-                    graph_color=color_alert,
-                    fill_color='{}.5'.format(color_alert),
-                    border_color=color_frame,
-                    line_width=2,
-                    border_width=1,
-                    samples=40,
-                    width=50,
-                    ),
-                # widget.Net(
-                #    interface = "wlp0s20f3",
-                #    format = 'Net: {down} ↓↑ {up}',
-                #    padding = 5
-                #    ),
-                Battery(
-                    charge_char = u'▲',
-                    discharge_char = u'▼',
-                    low_foreground = color_alert,
-                    ),
-                widget.QuickExit(),
-            ],
-            32,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        top=bar.Bar(top_bar,
+            24,
+        ),
+        bottom=bar.Bar(bottom_bar,
+            24,
         ),
     ),
 ]
