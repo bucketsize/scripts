239a1924a4ca7524baeac2f681dd979ee3bbc0e3
jb <jb@debugger2.net.loco>
2022-04-12 21:22:41 +0530

    xmonad workable

--- config/xmonad/my/xmonad.hs
+++ config/xmonad/my/xmonad.hs
@@ -1,35 +1,83 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
+import Control.Monad (liftM)
 import XMonad
+import XMonad.Core
+import System.Exit
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Grid
+import XMonad.Layout.Magnifier
+import XMonad.Layout.TabBarDecoration
+import XMonad.Hooks.DynamicLog hiding (shorten)
+import XMonad.Actions.CycleWS
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Actions.DwmPromote
+import XMonad.Actions.UpdatePointer
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.Run (spawnPipe)
+import System.IO 
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Util.WorkspaceCompare
+import XMonad.Util.EZConfig
+import XMonad.Actions.Warp
+import Data.Ratio
+import Control.Monad (liftM, forM_)
+import XMonad
+import XMonad.Core
+import System.Exit
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Grid
+import XMonad.Layout.Spiral
+import XMonad.Layout.Magnifier
+import XMonad.Layout.TabBarDecoration
+import XMonad.Layout.TwoPane
+import XMonad.Layout.NoBorders
+import XMonad.Layout.WindowNavigation
+import XMonad.Layout.Spacing
+import XMonad.Layout.SimpleFloat
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.Hooks.DynamicLog hiding (shorten)
+import XMonad.Actions.CycleWS
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Actions.DwmPromote
+import XMonad.Actions.UpdatePointer
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.Run (spawnPipe)
+import System.IO 
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.ConfirmPrompt
+import XMonad.Util.WorkspaceCompare
+import XMonad.Actions.Warp
+import Data.Ratio
 import XMonad.Util.Paste
 import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
-import XMonad.Config.Desktop
-import XMonad.Config.Gnome
 import XMonad.Actions.WindowBringer
 import XMonad.Actions.GridSelect
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.GroupNavigation
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.Grid
-import XMonad.Layout.TwoPane
-import XMonad.Layout.NoBorders
-import XMonad.Layout.WindowNavigation
-import XMonad.Layout.Spiral
+import XMonad.Actions.FloatKeys
+import XMonad.Config.Desktop
 import Data.Ratio -- this makes the '%' operator available (optional)
-import XMonad.Layout.Spiral
-import XMonad.Layout.Grid
-import XMonad.Layout.Spacing
 import Control.Exception
-import Control.Monad
 import Data.Monoid
 import System.IO
 import System.Exit
 import Text.Printf
-import qualified XMonad.StackSet as W
-import qualified Data.Map as M
 
 -- import DBus.Notify
 
@@ -42,16 +90,31 @@ import qualified Data.Map as M
 --   notification <- notify client hello
 --   return 0
 
-vBoxDisplay = Monitor { output="DisplayPort-0", mode="1280x720", position= "0x0" }
-lenovo_eDP1 = Monitor { output="eDP-1", mode="1280x720", position= "0x0" }
-pi4Hdmi0 = Monitor { output="HDMI-A-0", mode="1280x720", position= "0x0" }
-pi4Hdmi1 = Monitor { output="HDMI-A-1", mode="1280x720", position= "0x0" }
+data Monitor = Monitor
+  { output :: String
+  , mode :: String
+  , position :: String
+  }
 
-autostarts =
+data Sound = Sound
+  { device :: String
+  , level :: Int
+  }
+
+monitorUp   m = printf "xrandr --output %s --mode %s --pos %s --rotate normal" (output m) (mode m) (position (m::Monitor))
+monitorDown m = printf "xrandr --output %s --off" (output m)
+vBoxDisplay = Monitor { output="DisplayPort-0", mode="1280x720", position="0x0"}
+lenovo_eDP1 = Monitor { output="eDP-1", mode="1280x720", position="0x0"}
+pi4Hdmi0 = Monitor { output="HDMI-A-0", mode="1280x720", position="0x0"}
+pi4Hdmi1 = Monitor { output="HDMI-A-1", mode="1280x720", position="0x0"}
+
+monitor00=monitorUp lenovo_eDP1 ++ " --primary"
+
+autostarts = 
   [("xrdb prefs","xrdb ~/.Xresources")
   ,("dbus session","dbus-launch --sh-syntax --exit-with-session")
   ,("dbus activation","dbus-update-activation-environment --systemd --all")
-  ,("video", monitorUp lenovo_eDP1 ++ " --primary")
+  ,("video", monitor00)
   ,("wallpaper get", "~/.luarocks/bin/mxctl.control fun getwallpaper")
   ,("wallpaper apply", "~/.luarocks/bin/mxctl.control fun applywallpaper")
   ,("compositer", "picom -cb")
@@ -59,7 +122,7 @@ autostarts =
   ,("music player daemon","mpd")
   ,("policykit ui","lxpolkit")
   ,("notification daemon","dunst")
-  ,("network applet","connman-gtk")
+  ,("network applet","connman-gtk --tray")
   ,("vbox integration","VBoxClient --clipboard")
   ,("steam client","flatpak run com.valvesoftware.Steam")
  ]
@@ -69,123 +132,90 @@ main = do
 
 start = do
   -- xmobar <- spawnPipe "xmobar"
-  xmonad $ ewmh $ docks gnomeConfig
+  xmonad $ ewmh $ desktopConfig
     { terminal          = "qterminal"
-      , focusFollowsMouse = True
+      , focusFollowsMouse = False
       , borderWidth       = 4
       , modMask           = mod1Mask
       , workspaces        = ["1", "2", "3", "4"]
       -- normalBorderColor = myNormalBorderColor
       -- focusedBorderColor = myFocusedBorderColor
-      -- keys = myKeys
-      , mouseBindings = oMouseBindings
+      ,keys = oKeys
+      -- , mouseBindings = oMouseBindings
       , layoutHook        = oLayoutHook
       , manageHook        = oManageHook
       -- handleEventHook = myEventHook
       , startupHook       = handleStartup
       -- , logHook           = oLogHook xmobar  >>  historyHook
+      , logHook           = historyHook
     }
-      `additionalKeysP` oAddlKeysP
 
-oMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
-    
-    -- mod-button1, Set the window to floating mode and move by dragging
-    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
-    
-    -- mod-button2, Raise the window to the top of the stack
-    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
-    
-    -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
-    
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
-    ]
-
--- https://hackage.haskell.org/package/xmonad-contrib-0.16/docs/XMonad-Util-EZConfig.html#v:mkKeymap
-oAddlKeysP =
-  [ ("C-M-r",      spawn "xmonad --recompile && xmonad --restart")
-    , ("M-l",        spawn "slock")
-    , ("M4-r",       spawn "dmenu_run -l 10")
-    , ("M4-w",       gotoMenuArgs ["-l", "10"])
-    -- , ("M-<KP_Tab>", goToSelected defaultGSConfig)
-    -- close focused window 
-    , ("M-<F4>",     kill)
-    , ("M4-c",  	   kill)
-    , ("<Print>",    spawn "import -window root ~/Pictures/$(date +%Y%m%dT%H%M%S).png")
-    , ("C-<Print>",  spawn "import ~/Pictures/$(date +%Y%m%dT%H%M%S).png")
-
-    -- Rotate through the available layout algorithms
-    , ("M-<Space>", sendMessage NextLayout)
-    , ("M-S-<Space>", sendMessage FirstLayout)
-
-    -- Resize viewed windows to the correct size
-    , ("M-S-r", refresh)
-
-    -- Move window focus
-    , ("M-j", windows W.focusDown)
-    , ("M-k", windows W.focusUp  )
-    , ("M-<Return>", windows W.focusMaster  )
-    
-    -- Swap window
-    , ("M-S-j", windows W.swapDown >> windows W.focusDown)
-    , ("M-S-k", windows W.swapUp >> windows W.focusUp)
-    , ("M-S-<Return>", windows W.swapMaster)
-
-    -- Resize the master area
-    , ("M-h", sendMessage Shrink)
-    , ("M-l", sendMessage Expand)
-    , ("M-S-h", sendMessage MirrorShrink)
-    , ("M-S-l", sendMessage MirrorExpand)
-
-    -- toggle float/sink
-    , ("M-t", withFocused $ windows . W.sink)
-
-    -- Change the number of windows in the master area
-    , ("M-,", sendMessage (IncMasterN 1))
-    , ("M-.", sendMessage (IncMasterN (-1)))
-
-    -- Toggle the status bar gap
-    , ("M-g", sendMessage ToggleStruts)
+oKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+  [ 
+    -- Restart after compile
+    ((modm .|. controlMask, xK_r),      spawn "xmonad --recompile && xmonad --restart")
 
     -- Quit xmonad
-    , ("M-S-q", io (exitWith ExitSuccess))
+    , ((modm .|. controlMask, xK_q), io (exitWith ExitSuccess))
 
-    -- Restart xmonad
-    , ("M-q", restart "xmonad" True)
+    , ((modm, xK_l),        spawn "slock")
+    , ((mod4Mask, xK_r),       spawn "dmenu_run -l 10")
+    , ((mod4Mask, xK_w),       gotoMenuArgs ["-l", "10"])
+
+    -- window seitcher alt-tab style
+    -- , ((modm, xK_Tab), goToSelected defaultGSConfig)
+    -- , ((modm, xK_Tab), nextMatch Backward (return True))
+    , ((modm, xK_Tab), windows W.focusDown)
+    , ((modm .|. shiftMask, xK_Tab), windows W.focusUp)
+
+    -- close focused window 
+    , ((modm, xK_F4),kill)
+    , ((mod4Mask, xK_c),kill)
+    , ((0, xK_Print), spawn "import -window root ~/Pictures/$(date +%Y%m%dT%H%M%S).png")
+    , ((controlMask, xK_Print),spawn "import ~/Pictures/$(date +%Y%m%dT%H%M%S).png")
+
+    -- Rotate through the available layout algorithms
+    , ((modm, xK_space), sendMessage NextLayout)
+
+    -- Move window focus
+    , ((modm, xK_j), windows W.focusDown)
+    , ((modm, xK_k), windows W.focusUp  )
+    
+    -- Resize the master area
+    , ((modm ,xK_h), sendMessage Shrink)
+    , ((modm ,xK_l), sendMessage Expand)
+
+    -- toggle float/sink
+    , ((modm,xK_t), withFocused $ windows . W.sink)
+
+    -- Change the number of windows in the master area
+    , ((modm ,xK_comma), sendMessage (IncMasterN 1))
+    , ((modm ,xK_period), sendMessage (IncMasterN (-1)))
+
+    -- Toggle the status bar gap
+    , ((modm ,xK_g), sendMessage ToggleStruts)
+
+    , ((modm .|. controlMask, xK_l),withFocused (keysResizeWindow (-10,-10) (1,1)))
+    , ((modm .|. controlMask, xK_h),withFocused (keysResizeWindow (10,10) (1,1)))
+    , ((modm .|. shiftMask, xK_l),withFocused (keysMoveWindowTo (512,384) (1%2,1%2)))
   ]
 
-oManageHook = composeAll
-  [ className =? "MPlayer" --> doFloat
-    , className =? "Pidgin" --> doFloat
-    , className =? "Conky" --> doFloat
-    , className =? "Dunst" --> doFloat
-    , className =? "Pavucontrol" --> doFloat
-    , className =? "Steam" --> doFloat
-    , className =? "Gimp" --> doFloat
-    , resource  =? "gpicview" --> doFloat
-    , className =? "Mpv" --> doFloat
-    , className =? "Nm-connection-editor" --> doFloat
-    , className =? "arandr" --> doFloat
-    , resource  =? "desktop_window" --> doIgnore
-    , className =? "Stardict"        --> doFloat
-    , className =? "Smplayer"        --> doFloat
-    , className =? "Wicd-client.py"  --> doFloat
-    , className =? "Gxmessage"       --> doFloat
-    , className =? "Pidgin"          --> doShift "4"
-    , className =? "Conkeror"        --> doShift "2"
-    , className =? "Emacs"           --> doShift "3"
-    , className =? "Shiretoko"       --> doShift "2"
-    , className =? "Gvim"            --> doShift "3"
-    , resource  =? "desktop_window"  --> doIgnore
-    , className =? "stalonetray"     --> doIgnore 
-  ]
-    <+> manageDocks
+oManageHook = composeAll . concat $
+    [ [ className =? c               --> doFloat | c <- cFloats ]
+    , [ title     =? t               --> doFloat | t <- tFloats ]]
+  where cFloats = [ "Gimp", "Pidgin", "ROX-Filer", "Dunst", "Conky"
+                  , "Pavucontrol", "Steam", "Mpv", "arandr", "Gvim"]
+        tFloats = [ "Firefox Preferences", "Downloads", "Add-ons"
+                  , "Rename", "Create" ]
 
 oLayoutHook = avoidStruts
           $ configurableNavigation (navigateColor "#00aa00")
           $ smartBorders
           $ spacing 4 
-          $ Tall 1 (3/100) (1/2) ||| Grid ||| Full
+          $ simpleFloat 
+          ||| Tall 1 (3/100) (1/2) 
+          ||| spiral (125 % 146)
+          ||| Full
           ||| layoutHook desktopConfig
 
 oLogHook statProc =
@@ -203,19 +233,5 @@ handleStartup = do
   forM_ autostarts $ \(name, cmd) ->
     (spawn cmd) >> liftIO (print ("started" ++ name))
   -- liftIO (notifySend "XMonad" "started")
-  setWMName "LG3D" -- java compat
-
-data Monitor = Monitor
-  { output :: String
-  , mode :: String
-  , position :: String
-  }
-
-data Sound = Sound
-  { device :: String
-  , level :: Int
-  }
-
-monitorUp   m = printf "xrandr --output %s --mode %s --pos %s --rotate normal" (output m) (mode m) (position m)
-monitorDown m = printf "xrandr --output %s --off" (output m)
+  -- setWMName "LG3D" -- java compat
 
