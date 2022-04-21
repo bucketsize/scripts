a17d2a50d3920172d56e0ee5d34b7837ebbc8975
jb <jb@debugger2.net.loco>
2022-04-21 23:14:49 +0530

    more mxctl, fonts

--- config/config-global.lua
+++ config/config-global.lua
@@ -3,7 +3,7 @@ return {
 	font                  = "Cantarell",
 	font_size             = "12",
     font_monospace        = "Terminus",
-    font_monospace_size   = "12",
+    font_monospace_size   = "16",
     font_family_monospace = "Terminus",
     font_family_serif     = "Cantarell",
     font_family_sans      = "Cantarell"
