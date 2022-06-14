13ed0516dd7db5c4d51b1987d7f710cfc9e4cb40
jb <jb@debugger2.net.loco>
2022-06-14 19:01:19 +0530

    font change

--- config/config-global.lua
+++ config/config-global.lua
@@ -5,9 +5,9 @@ return {
     home				  = os.getenv("HOME"),
 	font                  = "DejaVu",
 	font_size             = "10",
-	font_monospace        = "Tamzen", -- "Cozette", -- glyphs don't match cozette
-	font_monospace_size   = "12",
-	font_family_monospace = "Tamzen", -- "Cozette",
+	font_monospace        = "dejavu sans mono", -- "Tamzen", -- "Cozette", -- glyphs don't match cozette
+	font_monospace_size   = "10",
+	font_family_monospace = "dejavu sans mono", --"Tamzen", -- "Cozette",
 	font_family_serif     = "DejaVu Serif",
     font_family_sans      = "DejaVu Sans",
 	openbox_theme 		  = "Nightmare-01", -- "Arc-Dark, Arc",
