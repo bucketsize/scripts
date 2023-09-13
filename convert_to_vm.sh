87e500d19219c059cf34a3da841b125a4e43fb91
jb <jb@debugger-m.jb>
2023-09-13 07:17:05 +0530

    nvim from flatpak

--- convert_to_vm.sh
+++ convert_to_vm.sh
@@ -5,10 +5,10 @@
 # Configure this as you require
 #-------------------------------------------------
 IMG_format=
-IMG_name="t_debian8_jessie"
+IMG_name="jb_11235"
 IMG_size=24
 
-LNX_ver=`uname -r`
+LNX_ver=$(uname -r)
 LNX_image="vmlinuz-$LNX_ver"
 LNX_initrd="initrd.img-$LNX_ver"
 LNX_rootdev="/dev/sda" #works on qemu and virtualbox
@@ -20,244 +20,76 @@ dryRun=false
 #WRKDIR="/media/domnic/tmp1"
 WRKDIR="/media/jb/83460770-be50-4a0f-bee1-113eac851f93/mnt/deb_bak-fuh_home/"
 
-create_img(){
-  IMG="$WRKDIR/$IMG_name.$IMG_format"
-  echo "[I] create => $IMG"
+cleanup_nbd() {
+	if [ -a /dev/nbd0 ]; then
+		qemu-nbd -d /dev/nbd0
+		rmmod nbd
+	fi
+	modprobe nbd max_part=16
+}
+create_target() {
+	IMG="$WRKDIR/$IMG_name.$IMG_format"
+	echo "#create_img $IMG"
+	if [ -a "$IMG" ]; then
+		echo "[W] [$IMG] already exists... skipped!"
+	else
+		qemu-img create -f raw $IMG ${IMG_size}G
+	fi
 
-  if [ -a "$IMG" ]; then
-    echo "[W] [$IMG] already exists... skipped!"
-  else
-    if [ "vdi" == "$IMG_format" ]; then
-      VBoxManage createhd --filename $IMG --size $(( IMG_size * 1024 ))
-    else
-      qemu-img create -f raw $IMG ${IMG_size}G
-    fi
-  fi
-}
-prepare_img_pre(){
-  IMG="$WRKDIR/$IMG_name.$IMG_format"
-  echo "[I] preparing => $IMG"
-
-  if [ "vdi" == "$IMG_format" ]; then
-    cleanup_nbd
-    qemu-nbd -c /dev/nbd0 $IMG
-    sleep 1
-    dpart /dev/nbd0
-    sleep 1
-    qemu-nbd -d /dev/nbd0
-  else
-    losetup /dev/loop0 $IMG
-    sleep 1
-    dpart /dev/loop0      
-    sleep 1
-    losetup -d /dev/loop0
-  fi
-}
-prepare_img_post(){
-  IMG="$WRKDIR/$IMG_name.$IMG_format"
-  echo "[I] preparing post_part => $IMG"
-  if [ "vdi" == "$IMG_format" ]; then
-    cleanup_nbd
-    sleep 1
-    qemu-nbd -c /dev/nbd0 $IMG
-    sleep 1
-    dformat /dev/nbd0
-  else
-    losetup /dev/loop0 $IMG
-    sleep 1
-    dformat /dev/loop0
-  fi
-}
-prepare_img_post2(){
-  IMG="$WRKDIR/$IMG_name.$IMG_format"
-  echo "[I] preparing post_part => $IMG"
-  if [ "vdi" == "$IMG_format" ]; then
-    cleanup_nbd
-    qemu-nbd -c /dev/nbd0 $IMG
-  else
-    losetup /dev/loop0 $IMG
-  fi
-}
-cleanup_img_post(){
-  IMG="$WRKDIR/$IMG_name.$IMG_format"
-  if [ "vdi" == "$IMG_format" ]; then
-    qemu-nbd -d /dev/nbd0
-  else
-    losetup -d /dev/loop0
-  fi
-}
-mount_img(){
-  echo "[I] mount => $TMPDIR"
-  boot=
-  root=
-  if [ "vdi" == "$IMG_format" ]; then
-    prepare_img_post2
-    boot=/dev/nbd0p1
-    root=/dev/nbd0p5
-  else
-    prepare_img_post2
-    boot=/dev/loop0p1
-    root=/dev/loop0p5
-  fi
-  disk_mount $root $boot $TMPDIR
-}
-umount_img(){
-  echo "[I] umount => $TMPDIR"
-  boot=
-  root=
-  if [ "vdi" == "$IMG_format" ]; then
-    boot=/dev/nbd0p1
-    root=/dev/nbd0p5
-  else
-    boot=/dev/loop0p1
-    root=/dev/loop0p5
-  fi
-  disk_umount $root $boot
-}
-clone_img(){
-  echo "[I] clone => $TMPDIR (dryRun=$dryRun)"
-  if [ "false" == "$dryRun" ]; then
-    rsync1_cmd / $TMPDIR
-  fi
-}
-install_bootloader(){
-  hdd=
-  boot=
-  root=
-  if [ "vdi" == "$IMG_format" ]; then
-    hdd=/dev/nbd0
-    boot=/dev/nbd0p1
-    root=/dev/nbd0p5
-  else
-    hdd=/dev/loop0
-    boot=/dev/loop0p1
-    root=/dev/loop0p5
-  fi
-  grub_install `uname -r` $hdd /dev/sda5 /dev/sda1 /dev/sda $TMPDIR  
-}
-cleanup_old(){
-  cleanup_oldconfigs $TMPDIR
-}
-start_vm(){
-  IMG="$WRKDIR/$IMG_name.$IMG_format"
-  echo "[I] vm => $IMG"
-
-  qemu-system-i386 \
-    -display sdl \
-    -soundhw ac97 \
-    -vga cirrus \
-    $IMG
+	echo "#mount_img $IMG"
+	losetup -d /dev/loop0
+	sleep 1
+	losetup /dev/loop0 $IMG
+	sleep 1
 }
 
-to_vdi(){
-  IMG="$WRKDIR/$IMG_name.$IMG_format"
-  IMG_vdi="$WRKDIR/$IMG_name.vdi"
-  echo "[I] converting $IMG => $IMG_vdi"
+prepare_target() {
+	dev=$1
+	echo "#prepare_target $dev"
+	partition $dev
+	sleep 1
 
-  if [ -f $IMG_vdi ]; then
-    echo "[W] already exists: $IMG_vdi"
-    rm $IMG_vdi
-  fi
-  echo "[I] vm => $IMG => $IMG_vdi"
-
-  #VBoxManage convertfromraw --format VDI [filename].img [filename].vdi
-  time VBoxManage convertfromraw --format VDI $IMG $IMG_vdi
+	echo "#format_img $IMG"
+	format $dev
+	sleep 1
 }
 
-resize_vdi(){
-  RESIZE=$1
-  IMG_vdi="$WRKDIR/$IMG_name.vdi"
-  echo "[I] resizing => $IMG_vdi => $RESIZE"
-
-  #VBoxManage modifyhd <absolute path to file> --resize <size in MB>
-  VBoxManage modifyhd $IMG_vdi --resize $RESIZE
-
-  #TODO mount and resize fs
+mount_target() {
+	tmpdir=$1
+	boot=$2
+	root=$3
+	echo "#mount_fs -> $tmpdir"
+	disk_mount $root $boot $tmpdir
 }
 
-# main
+umount_target() {
+	tmpdir=$1
+	boot=$2
+	root=$3
+	echo "#mount_fs -> $tmpdir"
+	disk_umount $root $boot
+}
+
+copy_files() {
+	tmpdir=$1
+	rsync_cmd / $tmpdir
+}
+
+install_bootloader() {
+	tmpdir=$1
+	boot=$2
+	root=$3
+	grub_install $(uname -r) $hdd /dev/sda5 /dev/sda1 /dev/sda $TMPDIR
+}
+start_vm() {
+	IMG="$WRKDIR/$IMG_name.$IMG_format"
+	echo "[I] vm => $IMG"
+
+	qemu-system-x86 \
+		-display sdl \
+		-soundhw ac97 \
+		-vga cirrus \
+		$IMG
+}
 
 set -e
-
-source ./sysbackup.sh 
-source ./validations.sh 
-
-validate "vbox"
-validate "qemu"
-validate "parted"
-validate "extlinux"
-
-cleanup_nbd(){
-  if [ -a /dev/nbd0 ]; then
-    qemu-nbd -d /dev/nbd0 
-    rmmod nbd
-  fi
-  modprobe nbd max_part=16
-}
-case $2 in
-  vdi)
-    IMG_format=$2
-    ;;
-  raw)
-    IMG_format=raw
-    ;;
-  *)
-    echo "[E] must specify vm format [vdi, raw]"
-    exit -1
-    ;;
-esac
-case $1 in
-  start_vm)
-    start_vm
-    ;;
-  to_vdi)
-    to_vdi
-    ;;
-  mount_dev)
-    disk_mount $2 $3 $TMPDIR 
-    ;;
-  umount_dev)
-    disk_umount $2 $3 
-    ;;
-  mount_img)
-    mount_img
-    ;;
-  umount_img)
-    umount_img
-    ;;
-  install_boot)
-    install_bootloader 
-    ;;
-  update)
-    prepare_img_post
-    mount_img
-    clone_img
-    cleanup_oldconfigs
-    install_bootloader
-    umount_img
-    cleanup_img_post
-    ;;
-  migrate)
-    create_img
-    prepare_img_pre
-    prepare_img_post
-    mount_img
-    clone_img
-    cleanup_old
-    install_bootloader
-    umount_img
-    cleanup_img_post
-    ;;
-  resize_vdi)
-    resize_vdi $2
-    ;;
-  cleanup_old)
-    cleanup_old
-    ;;
-  cleanup)
-    cleanup_img_post
-    ;;
-  *)
-    echo "[?] unknown command."
-    ;;
-esac
--- sysbackup.sh
+++ sysbackup.sh
@@ -1,202 +1,158 @@
 #!/bin/bash
 
-source ./validations.sh 
+source ./validations.sh
 
-cmd(){
-  echo "CMD> $1"
-  `$1`
-  if [ "x$?" != "x0" ]; then
-    if [ "x$2" == "xfalse" ]; then
-      echo "[E] cmd failed, cleanup manually"
-    fi
-  fi
+cmd() {
+	echo "CMD> $1"
+	$($1)
+	if [ "x$?" != "x0" ]; then
+		if [ "x$2" == "xfalse" ]; then
+			echo "[E] cmd failed, cleanup manually"
+		fi
+	fi
 }
-dpart(){
-  pdev=$1
-  echo "[I] creating partitions on [$pdev] for fresh isntall" 
-  validate parted ${pdev}
-  parted -s ${pdev} mklabel msdos
-  parted -a optimal --script ${pdev} \
-    unit MiB \
-    mkpart primary ext2 0% 2% \
-    mkpart extended 2% 100% \
-    mkpart logical ext4 2% 100% \
-    set 1 boot on \
-    print \
-    quit
+dpart() {
+	pdev=$1
+	echo "[I] creating partitions on [$pdev] for fresh isntall"
+	validate parted ${pdev}
+	parted -s ${pdev} mklabel msdos
+	parted -a optimal --script ${pdev} \
+		unit MiB \
+		mkpart primary ext2 0% 2% \
+		mkpart extended 2% 100% \
+		mkpart logical ext4 2% 100% \
+		set 1 boot on \
+		print \
+		quit
 }
-dformat(){
-  echo "[I] formatting partitions on [$pdev] for fresh isntall" 
-  pdev=$1
+dformat() {
+	echo "[I] formatting partitions on [$pdev] for fresh isntall"
+	pdev=$1
 
-  ls -l ${pdev}p1
-  ls -l ${pdev}p5
+	ls -l ${pdev}p1
+	ls -l ${pdev}p5
 
-  validate parted $pdev
-  mkfs -t ext2 -F ${pdev}p1
-  mkfs -t ext4 -F ${pdev}p5
-}
-
-rsync1_cmd(){
-  # -a archive
-  # -A preserve acls
-  # -X preserve extended attributes
-  # -v verbosity
-  # --delete extraneous files from destination
-  # don't use --progress
-  ionice -c3 rsync \
-    -aAX \
-    --delete \
-    --info=progress2 \
-    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} \
-    "$1" "$2"
-}
-rsync2_cmd(){
-  RSYNC="ionice -c3 rsync"
-  RSYNC_ARGS="-vrltD --delete --stats --human-readable"
-  RSYNC_ARGS="$RSYNC_ARGS --exclude={\"/dev/*\",\"/proc/*\",\"/sys/*\",\"/tmp/*\",\"/run/*\",\"/mnt/*\",\"/media/*\",\"/lost+found\",\"/home/*\"}"
-  SOURCES=$1
-  TARGET=$2
-
-  echo "Executing dry-run to see how many files must be transferred..."
-  TODO=$(${RSYNC} --dry-run ${RSYNC_ARGS} ${SOURCES} ${TARGET}|grep "^Number of files transferred"|awk '{print $5}')
-
-  ${RSYNC} ${RSYNC_ARGS} ${SOURCES} ${TARGET} | pv -l -e -p -s "$TODO"
-}
-fresh_install(){
-  backup=$1
-  blockDev=$2
-  source ./create_parts.sh 
-  dpart ${blockDev}
-
-  #guess created partitions
-  for i in `ls ${blockDev}*`; do
-    echo "$i"
-  done
-}
-disk_mount(){
-  root=$1
-  boot=$2
-  tmpd=$3
-
-  mkdir -p $tmpd
-  mount $root $tmpd
-
-  if [ "x" != "x$boot" ]; then
-    mkdir -p $tmpd/boot
-    mount $boot $tmpd/boot
-  fi
-}
-disk_umount(){
-  root=$1
-  boot=$2
-  tmpd=$3
-
-  umount $boot 
-  umount $root
-}
-part_install(){
-  backup=$1
-  rootDev=$2
-  bootDev=$3 #optional
-
-  echo "[I] installing ${backup} => boot:${bootDev} => root:${rootDev}"
-
-  rsync1_cmd $backup /mnt/tmp_root
-
-  cmd "install_extlinux `uname -r` ${rootDev} ${bootDev} /mnt/tmp_root"
-}
-grub_install(){
-  LNX_ver=$1
-  SDA_dev=$2  # /dev/nbd0
-  LNX_root=$3 # /dev/sda5
-  LNX_boot=$4 # /dev/sda1 + TODO include in fstab
-  LNX_dev=$5  # /dev/sda + asuming this is the first disk
-  LTMPDIR=$6
-
-  LNX_image=vmlinuz-$LNX_ver
-  LNX_initrd=initrd.img-$LNX_ver
-
-  cat > $LTMPDIR/boot/grub/grub.cfg <<-EOM
-set default="0"
-set timeout="5"
-insmod normal
-insmod linux
-set root=(hd0,msdos1)
-menuentry "$LNX_image" {
-  linux /$LNX_image root=$LNX_root rw
-  initrd /$LNX_initrd
-}
-EOM
-  cat $LTMPDIR/boot/grub/grub.cfg
-  
-  echo "[I] installing grub => $SDA_dev"
-  cmd "grub-install --boot-directory=$LTMPDIR/boot $SDA_dev"
-}
-extlinux_install(){
-  LNX_ver=$1
-  LNX_hdd=$2
-  LNX_rootdev=$3
-  LNX_bootdev=$4 #TODO include in fstab
-  LNX_boot_hdd=/dev/sda #asuming this is the first disk
-  LTMPDIR=$5
-
-  LNX_image=vmlinuz-$LNX_ver
-  LNX_initrd=initrd.img-$LNX_ver
-
-  #- install bootloader
-  EXTDIR=$LTMPDIR/boot/extlinux
-  if [ -d $LTMPDIR/boot/syslinux ]; then
-    EXTDIR=$LTMPDIR/boot/syslinux
-  elif [ -d $LTMPDIR/boot/extlinux ]; then
-    EXTDIR=$LTMPDIR/boot/extlinux
-  else 
-    mkdir -p $EXTDIR
-  fi
-  echo "[I] extlinux =>  $LNX_hdd -> $EXTDIR"
-  #- update bootloader config
-  #-- qemu default: root=/dev/sda
-  cat > $EXTDIR/extlinux.conf <<- EOM
-  DEFAULT $LNX_image
-  LABEL   $LNX_image
-  SAY     Booting - $LNX_image
-  LINUX   /boot/$LNX_image
-  INITRD  /boot/$LNX_initrd
-  APPEND  root=$LNX_boot_hdd rw
-EOM
-  cat $EXTDIR/extlinux.conf
-
-  extlinux --install $EXTDIR 
-
-  MBR_bin=
-  for i in `find /usr/lib -name mbr.bin`; do 
-    #take the first one
-    MBR_bin=$i
-    break;
-  done
-  echo "[I] extlinux = $MBR_bin"
-  dd if=${MBR_bin} conv=notrunc bs=440 count=1 of=${LNX_hdd}
+	validate parted $pdev
+	mkfs -t ext2 -F ${pdev}p1
+	mkfs -t ext4 -F ${pdev}p5
 }
 
-cleanup_oldconfigs(){
-  if [ x$1 == x ];then
-    echo "[E] invalid args"
-    exit -1
-  fi
-  suff=`date +%Y%m%d_%H%M`
-  LTMPDIR=$1
-  #-- remove invalid entries from $TMPDIR/etc/fstab
-  cmd "mv $LTMPDIR/etc/fstab $LTMPDIR/etc/fstab_$suff"
-  cat > $LTMPDIR/etc/fstab <<- EOM
-/dev/sda5   /       ext4    defaults    0   1
-/dev/sda1   /boot   ext2    defaults    0   1
-EOM
-  cat $LTMPDIR/etc/fstab 
+rsync1_cmd() {
+	# -a archive
+	# -A preserve acls
+	# -X preserve extended attributes
+	# -v verbosity
+	# --delete extraneous files from destination
+	# don't use --progress
+	ionice -c3 rsync \
+		-aAX \
+		--delete \
+		--info=progress2 \
+		--exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*"} \
+		"$1" "$2"
+}
+part_install() {
+	backup=$1
+	rootDev=$2
+	bootDev=$3 #optional
 
-  #-- let X detect video automatically
-  cmd "mv $LTMPDIR/etc/X11/xorg.conf $LTMPDIR/etc/X11/xorg.conf_$suff"
+	echo "[I] installing ${backup} => boot:${bootDev} => root:${rootDev}"
 
-  #-- existing user min restore
-  cmd "mkdir $LTMPDIR/home/jb"
-  cmd "chown jb.jb -R $LTMPDIR/home/jb"
+	rsync1_cmd $backup /mnt/tmp_root
+
+	cmd "install_extlinux $(uname -r) ${rootDev} ${bootDev} /mnt/tmp_root"
+}
+grub_install() {
+	LNX_ver=$1
+	SDA_dev=$2  # /dev/nbd0
+	LNX_root=$3 # /dev/sda5
+	LNX_boot=$4 # /dev/sda1 + TODO include in fstab
+	LNX_dev=$5  # /dev/sda + asuming this is the first disk
+	LTMPDIR=$6
+
+	LNX_image=vmlinuz-$LNX_ver
+	LNX_initrd=initrd.img-$LNX_ver
+
+	cat >$LTMPDIR/boot/grub/grub.cfg <<-EOM
+		set default="0"
+		set timeout="5"
+		insmod normal
+		insmod linux
+		set root=(hd0,msdos1)
+		menuentry "$LNX_image" {
+		  linux /$LNX_image root=$LNX_root rw
+		  initrd /$LNX_initrd
+		}
+	EOM
+	cat $LTMPDIR/boot/grub/grub.cfg
+
+	echo "[I] installing grub => $SDA_dev"
+	cmd "grub-install --boot-directory=$LTMPDIR/boot $SDA_dev"
+}
+extlinux_install() {
+	LNX_ver=$1
+	LNX_hdd=$2
+	LNX_rootdev=$3
+	LNX_bootdev=$4        #TODO include in fstab
+	LNX_boot_hdd=/dev/sda #asuming this is the first disk
+	LTMPDIR=$5
+
+	LNX_image=vmlinuz-$LNX_ver
+	LNX_initrd=initrd.img-$LNX_ver
+
+	#- install bootloader
+	EXTDIR=$LTMPDIR/boot/extlinux
+	if [ -d $LTMPDIR/boot/syslinux ]; then
+		EXTDIR=$LTMPDIR/boot/syslinux
+	elif [ -d $LTMPDIR/boot/extlinux ]; then
+		EXTDIR=$LTMPDIR/boot/extlinux
+	else
+		mkdir -p $EXTDIR
+	fi
+	echo "[I] extlinux =>  $LNX_hdd -> $EXTDIR"
+	#- update bootloader config
+	#-- qemu default: root=/dev/sda
+	cat >$EXTDIR/extlinux.conf <<-EOM
+		  DEFAULT $LNX_image
+		  LABEL   $LNX_image
+		  SAY     Booting - $LNX_image
+		  LINUX   /boot/$LNX_image
+		  INITRD  /boot/$LNX_initrd
+		  APPEND  root=$LNX_boot_hdd rw
+	EOM
+	cat $EXTDIR/extlinux.conf
+
+	extlinux --install $EXTDIR
+
+	MBR_bin=
+	for i in $(find /usr/lib -name mbr.bin); do
+		#take the first one
+		MBR_bin=$i
+		break
+	done
+	echo "[I] extlinux = $MBR_bin"
+	dd if=${MBR_bin} conv=notrunc bs=440 count=1 of=${LNX_hdd}
+}
+
+cleanup_oldconfigs() {
+	if [ x$1 == x ]; then
+		echo "[E] invalid args"
+		exit -1
+	fi
+	suff=$(date +%Y%m%d_%H%M)
+	LTMPDIR=$1
+	#-- remove invalid entries from $TMPDIR/etc/fstab
+	cmd "mv $LTMPDIR/etc/fstab $LTMPDIR/etc/fstab_$suff"
+	cat >$LTMPDIR/etc/fstab <<-EOM
+		/dev/sda5   /       ext4    defaults    0   1
+		/dev/sda1   /boot   ext2    defaults    0   1
+	EOM
+	cat $LTMPDIR/etc/fstab
+
+	#-- let X detect video automatically
+	cmd "mv $LTMPDIR/etc/X11/xorg.conf $LTMPDIR/etc/X11/xorg.conf_$suff"
+
+	#-- existing user min restore
+	cmd "mkdir $LTMPDIR/home/jb"
+	cmd "chown jb.jb -R $LTMPDIR/home/jb"
 }
