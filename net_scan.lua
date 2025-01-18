a5abafaad3289212bf23e4e1e132424cda33ab25
Herr bucketsize <bucket.size@gmail.com>
2025-01-18 20:17:03 +0530

    upd

--- net_scan.lua
+++ net_scan.lua
@@ -1,62 +1,55 @@
 #!/usr/bin/env lua
 
-require("luarocks.loader")
-package.path = "/?.lua;" .. package.path
-
-local Ut = require("minilib.util")
-
 local fn = {
 	disc = function(ip)
 		print("#discover network of", ip)
 		local neti, rptip, ipup = {}, nil, false
-		Ut:stream_exec(
-			string.format("sudo nmap -v -O --osscan-guess --osscan-limit --host-timeout 8 %s", ip),
-			function(x)
-				print(">", x)
-				local port, host = x:match("Discovered open port ([%w%p]+) on (.*)")
-				if host ~= nil and port ~= nil then
-					if neti[host] == nil then
-						neti[host] = { ports = {}, info = {} }
-					else
-						table.insert(neti[host].ports, port)
-					end
-				end
-
-				local rpts, s = x:match("Nmap scan report for (%d+.%d+.%d+.%d+) (.*)")
-				if rpts ~= nil and s:find("host down") == nil then
-					rptip, ipup = rpts, true
-					if neti[rptip] == nil then
-						neti[rptip] = { ports = {}, info = {} }
-					end
-				end
-
-				if rptip and ipup then
-					-- MAC Address: AE:72:4B:8B:DE:1C (Unknown)
-					-- Device type: general purpose
-					-- Running: Microsoft Windows 10
-					-- OS CPE: cpe:/o:microsoft:windows_10
-					-- OS details: Microsoft Windows 10 1709 - 1909
-					-- Network Distance: 1 hop
-					-- TCP Sequence Prediction: Difficulty=263 (Good luck!)
-					local mac = x:match("MAC Address: (.*)")
-					if mac then
-						table.insert(neti[rptip].info, mac)
-					end
-					local dev = x:match("Device type: (.*)")
-					if dev then
-						table.insert(neti[rptip].info, dev)
-					end
-					local os_run = x:match("Running: (.*)")
-					if os_run then
-						table.insert(neti[rptip].info, os_run)
-					end
-					local os_cpe = x:match("OS CPE: (.*)")
-					if os_cpe then
-						table.insert(neti[rptip].info, os_cpe)
-					end
+		local f = assert(io.popen("sudo nmap -v -O --osscan-guess --osscan-limit --host-timeout 8 " .. ip, "r"))
+		for x in f:lines("*l") do
+			print(">", x)
+			local port, host = x:match("Discovered open port ([%w%p]+) on (.*)")
+			if host ~= nil and port ~= nil then
+				if neti[host] == nil then
+					neti[host] = { ports = {}, info = {} }
+				else
+					table.insert(neti[host].ports, port)
 				end
 			end
-		)
+
+			local rpts, s = x:match("Nmap scan report for (%d+.%d+.%d+.%d+) (.*)")
+			if rpts ~= nil and s:find("host down") == nil then
+				rptip, ipup = rpts, true
+				if neti[rptip] == nil then
+					neti[rptip] = { ports = {}, info = {} }
+				end
+			end
+
+			if rptip and ipup then
+				-- MAC Address: AE:72:4B:8B:DE:1C (Unknown)
+				-- Device type: general purpose
+				-- Running: Microsoft Windows 10
+				-- OS CPE: cpe:/o:microsoft:windows_10
+				-- OS details: Microsoft Windows 10 1709 - 1909
+				-- Network Distance: 1 hop
+				-- TCP Sequence Prediction: Difficulty=263 (Good luck!)
+				local mac = x:match("MAC Address: (.*)")
+				if mac then
+					table.insert(neti[rptip].info, mac)
+				end
+				local dev = x:match("Device type: (.*)")
+				if dev then
+					table.insert(neti[rptip].info, dev)
+				end
+				local os_run = x:match("Running: (.*)")
+				if os_run then
+					table.insert(neti[rptip].info, os_run)
+				end
+				local os_cpe = x:match("OS CPE: (.*)")
+				if os_cpe then
+					table.insert(neti[rptip].info, os_cpe)
+				end
+			end
+		end
 		for i, j in pairs(neti) do
 			print(i)
 			print("ports: ")
@@ -70,14 +63,20 @@ local fn = {
 		end
 		return neti
 	end,
-
 	scan = function(ip)
 		print("#scanning")
-		Ut:stream_exec(string.format("nmap -T5 -A  %s", ip), function(x)
+		local f = assert(io.popen("nmap -T5 -A " .. ip, "r"))
+		for x in f:lines("*l") do
 			print(x)
-		end)
+		end
 	end,
-
+	fastscan = function(ip)
+		print("#fast scanning")
+		local f = assert(io.popen("nmap -sV --allports -T4 " .. ip, "r"))
+		for x in f:lines("*l") do
+			print(x)
+		end
+	end,
 	help = function()
 		print([[
     net_scan help       - displayes this help
